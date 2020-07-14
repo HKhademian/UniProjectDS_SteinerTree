e0a116b2cdfb76a3de5475a444391c9ce55ad8e8
Hossain Khademian <hco@post.com>
2020-07-14 12:35:57 +0430

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+	kotlin("jvm") version "1.3.72"
+}
+
+//group("ir.hkhademian")
+//version("1.0-SNAPSHOT")
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation (kotlin("stdlib-jdk8"))
+}
+
+//compileKotlin {
+//	kotlinOptions.jvmTarget = "1.8"
+//}
+//compileTestKotlin {
+//	kotlinOptions.jvmTarget = "1.8"
+//}
